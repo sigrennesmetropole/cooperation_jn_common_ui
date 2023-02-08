09d247c849909064ba27f3fedc159e247780dc15
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-02-08 16:40:37 +0100

    setup project test3

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import VueComponentNpmExample from '../lib/VueComponentNpmExample.vue'
+</script>
+
+<template>
+  <VueComponentNpmExample msg="Vue Component npm Example" />
+</template>
