15c8d180c293516ee6000a757b21af0e32347ef0
Ismail Sunni <imajimatika@gmail.com>
2023-02-10 22:41:31 +0700

    Add sample histoire and histoire config.

--- histoire.config.ts
+++ histoire.config.ts
@@ -3,4 +3,28 @@ import { HstVue } from '@histoire/plugin-vue'
 
 export default defineConfig({
   plugins: [HstVue()],
+  routerMode: 'hash',
+  vite: {
+    base: process.env.HISTOIRE_BASE || '/dist/',
+  },
+  tree: {
+    groups: [
+      {
+        id: 'homepage',
+        title: 'Homepage', // No toggle
+      },
+      {
+        id: 'map',
+        title: 'Map',
+      },
+      {
+        id: 'ui-base',
+        title: 'UI Base',
+      },
+      {
+        id: 'sidepanel',
+        title: 'Side Panel',
+      },
+    ],
+  },
 })
