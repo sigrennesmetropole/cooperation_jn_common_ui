0601b183f9f45fbbe9d28dd0074e65e9d78e338d
Ismail Sunni <imajimatika@gmail.com>
2023-02-16 20:12:49 +0700

    Update UI Buttons and add common icons

--- lib/main.ts
+++ lib/main.ts
@@ -3,5 +3,19 @@ import './css/main.css'
 import FooterArea from './FooterArea.vue'
 import UiAbstractTitle from './ui/UiAbstractTitle.vue'
 import LegalLink from './LegalLink.vue'
+import UiIconButton from './ui/UiIconButton.vue'
+import IconCompass from './icons/IconCompass.vue'
+import IconHome from './icons/IconHome.vue'
+import IconPlus from './icons/IconPlus.vue'
+import IconMinus from './icons/IconMinus.vue'
 
-export { FooterArea, UiAbstractTitle, LegalLink }
+export {
+  FooterArea,
+  UiAbstractTitle,
+  LegalLink,
+  UiIconButton,
+  IconCompass,
+  IconHome,
+  IconPlus,
+  IconMinus,
+}
