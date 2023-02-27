718ef4749647dded8c013733045b5f585bc97376
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-02-27 11:04:25 +0100

    add uiButtonWithTooltip

--- lib/main.ts
+++ lib/main.ts
@@ -5,6 +5,7 @@ import UiAbstractTitle from './ui/UiAbstractTitle.vue'
 import LegalLink from './LegalLink.vue'
 import UiIconButton from './ui/UiIconButton.vue'
 import UiCompass from './ui/UiCompass.vue'
+import UiButtonWithTooltip from './ui/UiButtonWithTooltip.vue'
 import IconCompass from './icons/IconCompass.vue'
 import IconHome from './icons/IconHome.vue'
 import IconPlus from './icons/IconPlus.vue'
@@ -16,6 +17,7 @@ export {
   UiCompass,
   LegalLink,
   UiIconButton,
+  UiButtonWithTooltip,
   IconCompass,
   IconHome,
   IconPlus,
