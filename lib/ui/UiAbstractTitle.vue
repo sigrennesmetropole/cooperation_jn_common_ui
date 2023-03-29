fdbcd73f3a686c68d0e850f473897784f4620546
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-03-29 17:14:37 +0200

    add dm sans

--- lib/ui/UiAbstractTitle.vue
+++ lib/ui/UiAbstractTitle.vue
@@ -18,9 +18,9 @@ defineProps({
 <template>
   <div class="flex items-center p-0 gap-2.5">
     <component v-if="logo" :is="logo" role="img" class="h-14 w-14"></component>
-    <h1 class="font-bold text-3xl text-center">{{ title }}</h1>
+    <h1 class="font-dm-sans font-bold text-3xl text-center">{{ title }}</h1>
   </div>
-  <div class="font-medium text-sm text-neutral-800">
+  <div class="font-dm-sans font-medium text-sm text-neutral-800">
     {{ abstract }}
   </div>
   <component v-if="panelSummary" :is="panelSummary"></component>
