f08af52beecf98ae163a7e7100c84ba1f15b7664
anaislet-c2c <anais.letellier@camptocamp.com>
2023-04-07 13:45:42 +0200

    293-accessibility-tooltip-buttons

--- lib/ui/UiIconButton.vue
+++ lib/ui/UiIconButton.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { Ref, ref } from 'vue'
+
 defineProps({
   active: {
     type: Boolean,
@@ -16,16 +18,44 @@ defineProps({
     type: String,
     default: '',
   },
+  height: {
+    type: String,
+  },
+  width: {
+    type: String,
+  },
+  positionX: {
+    type: String,
+  },
+  positionY: {
+    type: String,
+  },
 })
+
+const showTitle: Ref<boolean> = ref(false)
 </script>
 
 <template>
   <button
     :disabled="disabled"
-    class="shadow-lg w-12 h-12 bg-white flex items-center justify-center hover:bg-gray-100 focus:ring-2 focus:z-10 focus:ring-slate-400 focus:outline-none"
+    class="relative shadow-lg w-12 h-12 bg-white flex items-center justify-center hover:bg-gray-100 focus:ring-2 focus:z-10 focus:ring-slate-400 focus:outline-none"
     :title="titleButton"
     :aria-label="ariaLabelButton"
+    @focus="showTitle = true"
+    @blur="showTitle = false"
   >
+    <div
+      v-if="showTitle"
+      class="absolute border-2 border-black bg-white p-1 rounded text-black text-base font-normal"
+      :style="{
+        height: height + 'px',
+        width: width + 'px',
+        top: positionY + 'px',
+        left: positionX + 'px',
+      }"
+    >
+      {{ titleButton }}
+    </div>
     <slot></slot>
   </button>
 </template>
